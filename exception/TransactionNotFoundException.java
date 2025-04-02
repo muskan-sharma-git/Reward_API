a92de40d5afc145992d90ecaf40407606f201002
muskan-sharma-git <muskansharma707828@gmail.com>
2025-04-02 13:11:15 +0530

    Delete exception directory

--- exception/TransactionNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.home.work.exception;
-
-public class TransactionNotFoundException extends RuntimeException {
-	
-	public TransactionNotFoundException(String msg) {
-        super(msg);
-    }
-}
